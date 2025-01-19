fba6ba1d6ab0478d728b260499e9cab1c9a13294
PremBhandugare <prem.04.bhandugare@gmail.com>
2025-01-20 01:24:13 +0530

    api working

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require("../models/user");
-
+const bcryptjs = require("bcryptjs");
 const authRouter = express.Router();
 
 authRouter.post("/api/signup",async (req,res)=>{
@@ -14,10 +14,12 @@ authRouter.post("/api/signup",async (req,res)=>{
         return res.status(400).json({message:"Email already exists"});
     }
 
+    const hashedpass = await bcryptjs.hash(password,8);
+
     let user = new User({
         name,
         email,
-        password
+        password : hashedpass
     });
     user = await user.save();
     res.json(user);
